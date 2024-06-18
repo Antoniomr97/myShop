613a23ff99f21e7e7a18ce1fa0c7edfbe5d8d3c0
Antonio Martínez <AntonioMartinezRuiz1997@hotmail.com>
2024-06-18 19:44:51 +0200

    Final ProyectDone

--- myshop/front/products-manager/.next/static/chunks/webpack.js
+++ myshop/front/products-manager/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc1b3bbd72d5059e"; }
+/******/ 		__webpack_require__.h = function() { return "58394299c1aafd66"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- myshop/front/products-manager/src/components/createProductComponent/createProduct.jsx
+++ myshop/front/products-manager/src/components/createProductComponent/createProduct.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import styles from "./createProduct.module.css";
 import { CreateProductFetch } from "../../api/productFetch";
 
-export default function createProduct() {
+export default function CreateProduct() {
   const [image, setImage] = useState("");
   const [name, setName] = useState("");
   const [category, setCategory] = useState("");
   const [price, setPrice] = useState(0);
   const [score, setScore] = useState(0);
   const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
 
   const handlerOnChangeImage = (e) => {
     setImage(e.target.value);
@@ -31,14 +32,26 @@ export default function createProduct() {
   };
 
   const createNewProduct = async () => {
-    await CreateProductFetch({
-      image,
-      name,
-      category,
-      price,
-      score,
-    });
-    setMessage("Product Created");
+    // Validar que todos los campos estén llenos
+    if (!image || !name || !category || !price || !score) {
+      setError("Please fill in all fields.");
+      return;
+    }
+
+    try {
+      await CreateProductFetch({
+        image,
+        name,
+        category,
+        price,
+        score,
+      });
+      setMessage("Product Created");
+      setError(""); // Limpiar el mensaje de error si la creación fue exitosa
+    } catch (error) {
+      console.error("Error creating product:", error);
+      setError("Failed to create product.");
+    }
   };
 
   const containerStyle = {
@@ -48,6 +61,7 @@ export default function createProduct() {
     justifyContent: "center",
     alignItems: "center",
   };
+
   return (
     <div style={containerStyle}>
       <div className={styles.loginContainer}>
@@ -87,6 +101,7 @@ export default function createProduct() {
         <button className={styles.button} onClick={createNewProduct}>
           Create
         </button>
+        {error && <p className={styles.error}>{error}</p>}
         {message && <p>{message}</p>}
       </div>
     </div>
